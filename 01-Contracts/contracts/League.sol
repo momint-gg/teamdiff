530eb1837003d1fa9b64ba22e3dc595957998b46
Henry School Email <henryborska@wustl.edu>
2022-03-23 13:33:30 -0500

    Added logic for evaluating match on chain

--- 01-Contracts/contracts/League.sol
+++ 01-Contracts/contracts/League.sol
@@ -3,14 +3,69 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Athletes.sol";
 
-contract League is Ownable {
-    // State (proxy) vars:
+contract League is Ownable, Athletes {
+    // Vars
+    uint256 numWeeks = 10; // How long is a tournament?
+    uint256 leagueSize = 8;
+    uint256 currentWeekNum; // Keeping track of week number
+
     mapping(address => uint256) userToTotalPts;
     mapping(address => uint256[]) userToWeeklyPts;
+    mapping(address => uint256[]) userLineup;
 
     struct Matchup {
         address[2] players;
     }
     mapping(uint256 => Matchup[]) schedule; // Schedule for the league (generated before), maps week # => [matchups]
+
+    // Evaluating a match between two users (addresses)
+    // Returns which user won
+    function evaluateMatch(address addr1, address addr2)
+        public
+        onlyOwner
+        returns (address)
+    {
+        uint256[] memory lineup1 = userLineup[addr1];
+        uint256[] memory lineup2 = userLineup[addr2];
+        uint256 addr1Score;
+        uint256 addr2Score;
+
+        // Calculating users' total scores
+        for (uint256 i = 0; i < lineup1.length; i++) {
+            uint256[] memory currAthleteScores1 = athleteToScores[lineup1[i]];
+            uint256[] memory currAthleteScores2 = athleteToScores[lineup2[i]];
+            uint256 latestScore1 = currAthleteScores1[
+                currAthleteScores1.length - 1
+            ];
+            uint256 latestScore2 = currAthleteScores2[
+                currAthleteScores2.length - 1
+            ];
+            if (latestScore1 > latestScore2) {
+                addr1Score += 1;
+            } else {
+                addr2Score += 1;
+            }
+        }
+        // Incrementing week #
+        currentWeekNum += 1;
+        // Returning winner
+        if (addr1Score > addr2Score) return addr1;
+        else return addr2;
+    }
+
+    // Setting the lineup for a user
+    function setLineup(uint256[] memory athleteIds) public {
+        userLineup[msg.sender] = athleteIds;
+    }
+
+    // Returning the lineup for a user
+    function getLineup() public view returns (uint256[] memory) {
+        return userLineup[msg.sender];
+    }
+
+    // Sets the initial schedule for the league
+    // Randomly assigns matchups
+    function setLeagueSchedule() public onlyOwner {}
 }
