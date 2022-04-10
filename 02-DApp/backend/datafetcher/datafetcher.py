8c1e879931f26473bf27b4cf8123aa97073e0ec4
Isaiah Ware <isaiahware99@gmail.com>
2022-04-10 03:57:31 -0500

    game_stats from list to dict

--- 02-DApp/backend/datafetcher/datafetcher.py
+++ 02-DApp/backend/datafetcher/datafetcher.py
@@ -282,8 +282,6 @@ class DataFetcher():
                     self.athlete_game_stats[current_summoner_id][game_id][stat] = int(
                         value)
 
-        self.game_stats = [
-            {"summonerId": athlete, "games": games} for athlete, games in self.athlete_game_stats.items()]
         print("Done!\n")
 
     def aggregate_athlete_game_stats(self):
@@ -456,7 +454,7 @@ def main():
 
     df._convert_to_json(df.aggregated_athlete_game_stats,
                         "aggregated_stats.json")
-    df._convert_to_json(df.game_stats, "game_stats.json")
+    df._convert_to_json(df.athlete_game_stats, "game_stats.json")
 
     df.upload_headshots_to_pinata()
     df.fetch_athlete_headshot_data()
