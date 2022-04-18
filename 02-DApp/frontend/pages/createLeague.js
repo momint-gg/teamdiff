2402355841bb64c1d0ca5d9766d75c18f2e79d2c
reggiecai9 <44717548+reggiecai9@users.noreply.github.com>
2022-04-18 18:45:08 -0500

    checkpoint

--- 02-DApp/frontend/pages/createLeague.js
+++ 02-DApp/frontend/pages/createLeague.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
-import { Box, Typography, Button, Chip, Container, Paper, Fab } from "@mui/material";
+import { Box, Typography, Button, Chip, Container, Paper, Fab, OutlinedInput, styled, outlinedInputClasses  } from "@mui/material";
 import TextField from '@mui/material/TextField';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
@@ -8,7 +8,16 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import Input from '@mui/material/Input';
 import InputAdornment from '@mui/material/InputAdornment';
-import { useTheme } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid'
+
+const StyledOutlinedInput = styled(OutlinedInput)({
+  [`& .${outlinedInputClasses.focused} .${outlinedInputClasses.input}`]: {
+    borderColor: "green"
+  },
+  [`&:hover .${outlinedInputClasses.input}`]: {
+    color: "red"
+  },
+})
 
 export default function CreateLeague({setDisplay}) {
     const defaultValues = {
@@ -21,8 +30,6 @@ export default function CreateLeague({setDisplay}) {
 
     const [formValues, setFormValues] = useState(defaultValues)
 
-    const theme = useTheme()
-
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setFormValues({
@@ -32,7 +39,7 @@ export default function CreateLeague({setDisplay}) {
       };
 
     return (
-        <Box sx={{backgroundColor: theme.palette.primary.main}}>
+        <Box sx={{backgroundColor: "primary.main"}}>
           <Fab variant="extended" size="small" color="primary" aria-label="add" onClick={() => setDisplay(false)}>
             &#60; BACK
           </Fab>
@@ -51,39 +58,61 @@ export default function CreateLeague({setDisplay}) {
                Fill out this form to create a league for you and your friends!
         </Typography>
 
-        <Box
-        component="form"
-        sx={{
-            '& > :not(style)': { m: 1, width: '25ch' },
-        }}
-        noValidate
-        autoComplete="off"
-        >
-        <TextField
-            label="League Name"
-            variant="standard"
-            color="secondary"
-            backgroundColor="secondary"
-            
-            focused
-        />
-        </Box>
 
-        <FormControl >
-            <InputLabel id="demo-simple-select-label">Token</InputLabel>
-            <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={formValues.token}
-            label="token"
-            name="token"
-            onChange={handleInputChange}
-            >
-            <MenuItem key="eth" value="eth">eth</MenuItem>
-            <MenuItem key="usdc" value="usdc">usdc</MenuItem>
-            <MenuItem key="other" value="other">other</MenuItem>
-            </Select>
-        </FormControl>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            <Box
+              component="form"
+              sx={{
+                  '& > :not(style)': { m: 1, width: '25ch' },
+              }}
+              noValidate
+              autoComplete="off"
+              >
+                <FormControl>
+                  <InputLabel htmlFor="component-outlined">Name</InputLabel>
+                  <StyledOutlinedInput
+                    id="component-outlined"
+                    value={"we"}
+                    onChange={handleInputChange}
+                    label="Name"
+                  />
+                </FormControl>
+              {/* <TextField
+                required
+                label="League Name"
+                variant="standard"
+                color="secondary"
+                backgroundColor="secondary"
+                id='outlined-required'
+                // defaultValue="e.g. TeamDiff"
+              /> */}
+            </Box>
+            <FormControl >
+              <InputLabel id="demo-simple-select-label">Token</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={formValues.token}
+                label="token"
+                name="token"
+                onChange={handleInputChange}
+                >
+                <MenuItem key="eth" value="eth">eth</MenuItem>
+                <MenuItem key="usdc" value="usdc">usdc</MenuItem>
+                <MenuItem key="other" value="other">other</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid item xs={6}>
+          
+          </Grid>
+        </Grid>
+        
+        
+        
+
+        
 
         <FormControl  sx={{ marginLeft: 3 }} variant="standard">
           <InputLabel>Buy-In Cost</InputLabel>
