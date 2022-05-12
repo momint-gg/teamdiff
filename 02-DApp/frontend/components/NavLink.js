6400139a7335070ea065ea8a3c4d5698cb7c70ac
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2022-05-12 15:11:13 -0500

    fixed two warnings

--- 02-DApp/frontend/components/NavLink.js
+++ 02-DApp/frontend/components/NavLink.js
@@ -5,33 +5,32 @@ import { useRouter } from "next/router";
 export default function NavLink({ className, href, children }) {
   const router = useRouter();
   return (
-    <Link href={href} passHref prefetch>
+    <Link href={href} passHref>
       <a
-        style={router.pathname === href
-            ? 
-            {
-            color:"white",
-            fontSize: '40px',
-            textDecoration: 'underline',
-            fontFamily: 'Exo',
-            marginRight: '1%',
-            marginLeft: '1%',
-            textDecorationColor: 'cyan',
-            textUnderlineOffset: '10px',
-            }
-            : 
-            {
-            color:"white",
-            fontSize: '40px',
-            textDecoration: 'none',
-            fontFamily: 'Exo',
-            marginRight: '1%',
-            marginLeft: '1%'
-            }
+        style={
+          router.pathname === href
+            ? {
+                color: "white",
+                fontSize: "40px",
+                textDecoration: "underline",
+                fontFamily: "Exo",
+                marginRight: "1%",
+                marginLeft: "1%",
+                textDecorationColor: "cyan",
+                textUnderlineOffset: "10px",
+              }
+            : {
+                color: "white",
+                fontSize: "40px",
+                textDecoration: "none",
+                fontFamily: "Exo",
+                marginRight: "1%",
+                marginLeft: "1%",
+              }
         }
       >
         {children}
       </a>
     </Link>
   );
-};
+}
