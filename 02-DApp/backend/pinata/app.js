934312498cb9e9cdeda11489b5be203689b237cc
Henry School Email <henryborska@wustl.edu>
2022-05-25 23:07:24 -0700

    getting started on backend

--- 02-DApp/backend/pinata/app.js
+++ 02-DApp/backend/pinata/app.js
@@ -1,44 +1,44 @@
-const fs = require("fs");
-const pinataSDK = require("@pinata/sdk");
+require('dotenv').config();
+const fs = require('fs');
+const pinataSDK = require('@pinata/sdk');
 
-const PINATA_KEY = "";
-const SECRET_API_KEY = "";
+// ADD IN YOUR OWN KEYS HERE
+const PINATA_KEY = process.env.PINATA_KEY;
+const SECRET_API_KEY = process.env.PINATA_SECRET_KEY;
 
 const pinata = pinataSDK(PINATA_KEY, SECRET_API_KEY);
 
 async function uploadHeadshots(pathToHeadshotsDir) {
-    const headshots = fs.readdirSync(pathToHeadshotsDir);
+  const headshots = fs.readdirSync(pathToHeadshotsDir);
 
-    for (let i = 0; i < headshots.length; i++) {
-        console.log(`(Headshot ${i + 1}/${headshots.length}) ${headshots[i]}`);
-        const readableStreamForFile = fs.createReadStream(
-            pathToHeadshotsDir + `/${headshots[i]}`
-        );
-        let result = await pinata.pinFileToIPFS(readableStreamForFile);
-        console.log(result.IpfsHash + "\n");
-    }
+  for (let i = 0; i < headshots.length; i++) {
+    console.log(`(Headshot ${i + 1}/${headshots.length}) ${headshots[i]}`);
+    const readableStreamForFile = fs.createReadStream(
+      pathToHeadshotsDir + `/${headshots[i]}`
+    );
+    let result = await pinata.pinFileToIPFS(readableStreamForFile);
+    console.log(result.IpfsHash + '\n');
+  }
 }
 
 async function main() {
-    const auth = await pinata.testAuthentication();
+  const auth = await pinata.testAuthentication();
 
-    if (auth.authenticated === false) {
-        console.log(
-            "ERROR: Not authenticated with Pinata! Terminating upload..."
-        );
-        return;
-    }
-    
-    const args = process.argv.slice(2);
-    let pathToHeadshotsDir = "headshots";
+  if (auth.authenticated === false) {
+    console.log('ERROR: Not authenticated with Pinata! Terminating upload...');
+    return;
+  }
 
-    if (args.length === 1 && args[0] === "-p") {
-        // If calling this script from datafetcher.py, the headshot folder
-        // path is relative to datafetcher.py instead of pinata/app.js
-        pathToHeadshotsDir = "../pinata/headshots";
-    }
+  const args = process.argv.slice(2);
+  let pathToHeadshotsDir = 'headshots';
 
-    await uploadHeadshots(pathToHeadshotsDir);
+  if (args.length === 1 && args[0] === '-p') {
+    // If calling this script from datafetcher.py, the headshot folder
+    // path is relative to datafetcher.py instead of pinata/app.js
+    pathToHeadshotsDir = '../pinata/headshots';
+  }
+
+  await uploadHeadshots(pathToHeadshotsDir);
 }
 
 main();
