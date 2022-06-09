fa3003d4f6fc427d8bfa4f7886e386c461d94bf7
Henry School Email <henryborska@wustl.edu>
2022-06-09 00:15:18 -0700

    began merge with hnerys branch

---
+++ 02-DApp/backend/contractscripts/whitelist_control/allowBoosterPacks.js
@@ -0,0 +1,33 @@
+// Allowing booster packs to be opened (to be run on TBD)
+
+require('dotenv').config({ path: '../.env' });
+const { ethers } = require('ethers');
+const { GameItems } = require('../contract_info/contractAddresses');
+const GameItemsJSON = require('../contract_info/abis/GameItems.json');
+
+// TODO:
+const main = async () => {
+  // Constructing our contract
+  const provider = new ethers.providers.AlchemyProvider(
+    'matic',
+    process.env.MATIC_KEY
+  );
+  const rinkebySigner = new ethers.Wallet(process.env.OWNER_KEY, provider);
+  contract = new ethers.Contract(GameItems, GameItemsJSON.abi, rinkebySigner);
+
+  let txn = await contract.allowBoosterPacks();
+  await txn.wait();
+};
+
+const runMain = async () => {
+  try {
+    console.log('Running main...\n');
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
