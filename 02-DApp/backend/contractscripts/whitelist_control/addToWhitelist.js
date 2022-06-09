d27b9194ed9d93cac5c64cee4f32d1447886e9d8
Henry School Email <henryborska@wustl.edu>
2022-06-09 11:54:52 -0700

    debugging WL

---
+++ 02-DApp/backend/contractscripts/whitelist_control/addToWhitelist.js
@@ -0,0 +1,75 @@
+// Lil script for adding people in excel sheet to whitelist
+require('dotenv').config({ path: '../.env' });
+const { ethers } = require('ethers');
+const { GameItems } = require('../contract_info/contractAddresses');
+const GameItemsJSON = require('../contract_info/abis/GameItems.json');
+const XLSX = require('xlsx');
+const web3 = require('web3');
+
+// TODO:
+const main = async () => {
+  // Constructing our contract
+  // TODO: Change to matic
+  const provider = new ethers.providers.AlchemyProvider(
+    'rinkeby',
+    process.env.ALCHEMY_KEY
+  );
+  const rinkebySigner = new ethers.Wallet(process.env.OWNER_KEY, provider);
+  contract = new ethers.Contract(GameItems, GameItemsJSON.abi, rinkebySigner);
+
+  // Reading WL in from excel
+  whitelist = []; // Users we're going to WL
+  const parseExcel = (filename) => {
+    const excelData = XLSX.readFile(filename);
+
+    return Object.keys(excelData.Sheets).map((name) => ({
+      name,
+      data: XLSX.utils.sheet_to_json(excelData.Sheets[name]),
+    }));
+  };
+
+  parseExcel('./TeamDiff Final WL.xlsx').forEach((element) => {
+    data = element.data;
+  });
+
+  // Adding to WL array
+  const firstAddy = '0x37D1431D5D423d66ad6F369EF1bB0767E71A8400';
+  whitelist.push(firstAddy);
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][firstAddy].length > 0) whitelist.push(data[i][firstAddy]);
+  }
+  console.log(whitelist);
+
+  // Adding users to whitelist
+  let txn = await contract.addUsersToWhitelist([...whitelist]);
+  await txn.wait();
+
+  // Checking length of whitelist
+  const numWhitelisted = await contract.getNumWhitelisted();
+  // const numWhitelisted = await contract.numWhitelisted();
+
+  Number(numWhitelisted) === data.length
+    ? console.log(
+        'Successfully added everyone to WL! Added ',
+        Number(numWhitelisted),
+        ' people.'
+      )
+    : console.log(
+        "Error adding people to WL. Lengths don't match. Length of WL in contract is ",
+        Number(numWhitelisted)
+      );
+};
+
+const runMain = async () => {
+  try {
+    console.log('Running main...\n');
+    await main();
+
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
