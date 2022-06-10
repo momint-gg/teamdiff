9590435bf7bf73b3aa3e63be63263631a66816be
reggiecai9 <44717548+reggiecai9@users.noreply.github.com>
2022-06-10 00:42:46 -0500

    collections renderings and modals

--- 02-DApp/frontend/components/AthleteCardModal.js
+++ 02-DApp/frontend/components/AthleteCardModal.js
@@ -4,11 +4,13 @@ import {
   Avatar,
   CardMedia,
   Typography,
-  Modal,
+  Modal
 } from "@mui/material";
+import { IoCloseOutline } from "react-icons/io5";
 
 export default function AthleteCardModal({
   modalOpen,
+  image,
   athleteData,
   handleModalClose,
 }) {
@@ -30,27 +32,52 @@ export default function AthleteCardModal({
           borderTop: "10%",
           borderLeft: "10%",
           width: "50%",
-          height: "75%",
-          backgroundColor: "primary.charcoal",
+          height: "90%",
+          minWidth: 450,
+          backgroundColor: "primary.main",
           boxShadow: 24,
           borderRadius: "10%",
           display: "flex",
           flexWrap: "wrap",
           justifyContent: "center",
-          padding: 8,
+          padding: 2,
         }}
       >
+        <Button
+          style={{ position: "absolute", top: "10px", right: "10px" }}
+          onClick={handleModalClose}
+        >
+          <IoCloseOutline 
+          size={"3rem"} 
+          onClick={() => {
+            handleModalClose()
+            }} 
+          style={{ cursor: "pointer",
+                   position: "absolute",
+                   right: "5px",
+                   top: "5px",
+                   color: "black"
+                }}
+          />
+        </Button>
         <Typography fontSize={30}>{athleteData?.title}</Typography>
         <CardMedia
           component="img"
           height="400"
-          image={athleteData?.metadata.image}
+          image={image}
           alt="NFT Image"
+          sx={{
+            objectFit: "contain"
+          }}
         />
-        <Typography variant="title" color="inherit" noWrap>
+        {/* <Typography variant="title" color="inherit" noWrap>
           &nbsp;
-        </Typography>
-        <Typography variant="body1" color="inherit">
+        </Typography> */}
+        <Typography variant="body1" color="inherit" sx={{
+          pl: 5,
+          pr: 5,
+          textAlign: "center"
+        }}>
           {athleteData?.description}
         </Typography>
       </Box>
