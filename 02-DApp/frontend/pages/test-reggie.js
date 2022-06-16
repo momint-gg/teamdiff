c6366a79dc07b920f09a1da63b4af38f902d9a3c
reggiecai9 <44717548+reggiecai9@users.noreply.github.com>
2022-06-16 00:01:51 -0500

    Delete test-reggie.js

--- 02-DApp/frontend/pages/test-reggie.js
+++
@@ -1,9 +0,0 @@
-import PlayerStateModal from "../components/PlayerStateModal";
-
-export default function TestReggie () {
-    return (
-        // <div>hello</div>
-        <PlayerStateModal modalOpen={true} playerName={'shenyi'} 
-        handleModalClose={() => console.log('close')} />
-    )
-} 
