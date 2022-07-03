08f244c6ddd702e16a606170da7e2067557478dc
lrudolph333 <trey.ru.iii@gmail.com>
2022-07-02 18:30:16 -0700

    uncommenting play page

--- 02-DApp/frontend/components/Layout.js
+++ 02-DApp/frontend/components/Layout.js
@@ -8,7 +8,7 @@ import NavLink from "./NavLink";
 
 const Layout = ({ children, isMobile }) => {
   const pages = [
-    // { name: "PLAY", href: "/play" },
+    { name: "PLAY", href: "/play" },
     { name: "MY COLLECTION", href: "/collection" },
     { name: "MINT", href: "/mintHome" },
     { name: "OPEN", href: "/burnPack" },
